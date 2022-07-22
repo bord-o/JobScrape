7d2c909fa490c6412641ae8bb60f106a94d215ee
bordo <bordo@debian-BULLSEYE-live-builder-AMD64>
2022-07-22 09:47:35 -0400

    parallel map working with multiprocessing

--- asyncReq.py
+++ asyncReq.py
@@ -1,24 +1,18 @@
 import httpx
-import asyncio
-
-
-async def getpls(urls):
-    async with httpx.AsyncClient() as client:
-        req = await client.get(urls)
-    return req
-
+import multiprocessing
 
 test_jobs = ['https://www.workatastartup.com/companies/focal-systems', 'https://www.workatastartup.com/companies/daily', 'https://workatastartup.com/companies/mason', 'https://workatastartup.com/companies/magicbus', 'https://workatastartup.com/companies/tovala']
 
 
-async def run(urls):
-    tasks = []
-    for url in urls:
-        tasks.append(getpls(url))
-    html = await asyncio.gather(*tasks)
-    return html
+def getPg(urls):
+    with multiprocessing.Pool() as p:
+        res = p.map(httpx.get, urls)
+    return res
 
 
-def getReqs(urls):
-    a = asyncio.run(run(urls))
-    return a
+# just a sequential version of the function for testing
+def getPgSeq(urls):
+    res = []
+    for u in urls:
+        res.append(httpx.get(u))
+    return res
--- jobscrape.py
+++ jobscrape.py
@@ -5,19 +5,18 @@ import asyncReq
 JOBS_URL = 'https://account.ycombinator.com/?continue=https%3A%2F%2Fwww.workatastartup.com%2F'
 JOBS_USER = 'brodylittle011@gmail.com'
 JOBS_PASS = '0nly?Consept'
-THREADS = 4
 
 HEADLESS = False
 
-test_jobs = ['https://www.workatastartup.com/companies/focal-systems', 'https://www.workatastartup.com/companies/daily', 'https://workatastartup.com/companies/mason', 'https://workatastartup.com/companies/magicbus', 'https://workatastartup.com/companies/tovala']
+test_jobs = ['https://www.workatastartup.com/companies/focal-systems', 'https://www.workatastartup.com/companies/daily', 'https://www.workatastartup.com/companies/mason', 'https://www.workatastartup.com/companies/magicbus', 'https://www.workatastartup.com/companies/tovala']
 
 
 def main():
-    urls = scrape.YCombLogin(JOBS_USER, JOBS_PASS, True, depth=2, writeFile=False)
+    urls = scrape.YCombLogin(JOBS_USER, JOBS_PASS, True, depth=16, writeFile=False)
     jobs = list(map(scrape.addwww, urls))
     print(f"total jobs: {len(jobs)}")
 
-    reqs = asyncReq.getReqs(jobs)
+    reqs = asyncReq.getPg(jobs)
 
     print(f"reqests recieved: {len(reqs)}")
     print(list(filter(lambda req: req.status_code != 200, reqs)))
@@ -30,6 +29,13 @@ def main():
     with open("./tech.txt", 'w+') as wf:
         for entry in tech:
             wf.write(f'{entry}\n---------------------------------------------------------------------------\n')
+
+    empty_tech = 0
+    for entry in tech:
+        if entry == "":
+            empty_tech += 1
+
+    print(f"{empty_tech} companies had no technology section. (out of {len(tech)})")
 
 if __name__ == "__main__":
     main()
